6bc72361e690bf00c8cd2e8d252fd574364a1178
Thao Dang JS <thaodang.js@gmail.com>
2024-01-06 17:10:24 +0700

    using enum in types.ts and creating new state to manage selected page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
+import { SelectedPage } from "./shared/types";
+
 function App() {
-  return;
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home,
+  );
+
+  return <div className="app bg-gray-20"></div>;
 }
 
 export default App;
